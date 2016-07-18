7de6b3ea7fffa477b21429a97f6768f79e90dba4
Jason Curl <jcurl@arcor.de>
2016-07-18 21:36:14 +0200

    Escape double quotes with a double quote.
    When exporting to CSV format, a double quote in the original text must be replaced with another double quote. If not, the CSV parser may see a semicolon and think that's a field separator.
    
    Tested by loading in LibreOffice a CSV using semicolons to separate. We see that only a single double quote is shown, even though in the source and the output two sets of double quotes are shown. The format contains both a quoted word in the middle and also at the end of the field.
    
    Issue: GNATS-Automation/637

--- EAExport/Model/CsvDoorsTreePlainExport.cs
+++ EAExport/Model/CsvDoorsTreePlainExport.cs
@@ -45,8 +45,8 @@
                     string convertedText = ConvertHtmlToPlainText(new HtmlFormat(HtmlFormatMode.None), text);
                     m_Writer.WriteLine("{0};{1};\"{2}\";\"{3}\"",
                         element.Id, parentId,
-                        heading != null ? heading : string.Empty,
-                        convertedText != null ? convertedText : string.Empty);
+                        heading != null ? EscapeCsvText(heading) : string.Empty,
+                        convertedText != null ? EscapeCsvText(convertedText) : string.Empty);
                 }
             }
 
@@ -133,6 +133,27 @@
             foreach (HtmlNode child in node.ChildNodes) {
                 ParseHtml(format, child, sb);
             }
+        }
+
+        private static string EscapeCsvText(string text)
+        {
+            StringBuilder sb = null;
+            int l = text.Length;
+            int p = 0;
+            while (p < l) {
+                int cp = text.IndexOf('"', p);
+                if (p == 0 && cp == -1) return text;
+                if (sb == null) sb = new StringBuilder();
+
+                if (cp == -1) {
+                    sb.Append(text.Substring(p, l - p));
+                    p = l;
+                } else {
+                    sb.Append(text.Substring(p, cp + 1 - p)).Append('"');
+                    p = cp + 1;
+                }
+            }
+            return sb == null ? text : sb.ToString();
         }
 
         /// <summary>
